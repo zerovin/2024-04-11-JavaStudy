aa3089251bbfd273e450d2dae5cdeaa7b3c72ba5
sist <sist@DESKTOP-84RB4B8>
2024-05-28 17:55:53 +0900

    0528_공부법

---
+++ Chapter11_라이브러리(java.io)_3/src/com/sist/ann/BoardList.java
@@ -0,0 +1,5 @@
+package com.sist.ann;
+// ***어노테이션 / 열거형 / 람다식
+public class BoardList {
+	
+}
---
+++ Chapter11_라이브러리(java.io)_3/src/com/sist/io/라이브러리_1.java
@@ -0,0 +1,48 @@
+package com.sist.io;
+/*
+ *  프로그램
+ *    => 흐름파악 - 어떤화면으로 이동하는지 card.show(), <a href="">
+ *    => 문법 - 자바기본
+ *    => 조립 
+ *  ---------소스코딩을 숙달
+ *  
+ *  소스는 암기
+ *    - 변수명, 클래스명 => 암기가 안됨
+ *    - 순서를 가지고 순서대로 암기 => 알고리즘
+ *    
+ *  많이 써보고 사용위치 파악
+ *  사용위치
+ *    - Application 
+ *      - File 입출력
+ *        ObjectInputStream / ObjectOutputStream
+ *    - Web Application 
+ *      - 데이터베이스
+ *        SQL(오라클, MySQL=MariaDB) => 파일 사용없이
+ *        
+ *  라이브러리
+ *    java.lang
+ *    java.util
+ *    java.io
+ *    java.text
+ *    java.net
+ *    
+ *  객체 생성시 new사용하면 결합성이 높은 프로그램 => 가급적 new사용 지양
+ *    결합성이 높은 프로그램 => 에러시 다른 클래스에 영향이 있음
+ *      => new없이 객체 생성 => 리플렉션 => 스프링, 
+ *    메모리 누수 현상 방지
+ *      => 패턴 - 싱글턴
+ *         디자인 패턴 - GOF패턴 : 23가지 중 8가지 중요
+ *                    싱글턴 : 데이터베이스 => Connection => XE(커넥션 50개면 종료)
+ *                    팩토리패턴 : 클래스 여러개 관리
+ *                    어댑터패턴 : 자동 형변환
+ *                    MV패턴 : 데이터관리(Model-Back), 화면UI(View-Front) 나눠서 따로 작업 
+ *                    MVC패턴 : 데이터관리(Model), 화면UI(View), 컨트롤러(Controller) 
+ */
+public class 라이브러리_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
